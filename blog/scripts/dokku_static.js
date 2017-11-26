22b9e6be6b2c49fa59b3cff59adf67b1adf6dce6
Mateusz Derks <mateusz.derks@toptal.com>
2017-11-26 22:10:10 +0100

    Backward compatibility for rss URL

--- blog/scripts/dokku_static.js
+++ blog/scripts/dokku_static.js
@@ -2,6 +2,6 @@ const fs = require('fs')
 const path = require('path')
 
 // Add .static file to the root to make use of https://github.com/dokku/buildpack-nginx
-hexo.extend.filter.register('after_generate', function(){
+hexo.extend.filter.register('after_generate', function () {
   fs.writeFileSync(path.join(__dirname, '..', 'public', '.static'), '')
 });
