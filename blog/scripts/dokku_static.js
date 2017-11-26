992372392b513105e7670cdada1d79d5f10dc9fd
Mateusz Derks <mateusz.derks@toptal.com>
2017-11-26 21:34:06 +0100

    Add rss

--- blog/scripts/dokku_static.js
+++ blog/scripts/dokku_static.js
@@ -1,3 +1,7 @@
+const fs = require('fs')
+const path = require('path')
+
+// Add .static file to the root to make use of https://github.com/dokku/buildpack-nginx
 hexo.extend.filter.register('after_generate', function(){
-    console.log('Generate', arguments)
+  fs.writeFileSync(path.join(__dirname, '..', 'public', '.static'), '')
 });
