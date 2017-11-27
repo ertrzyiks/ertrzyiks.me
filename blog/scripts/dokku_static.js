68273dd0ef273f31cb1d4f058bfb666abc95e046
Mateusz Derks <mateusz.derks@toptal.com>
2017-11-27 20:21:43 +0100

    Add 404 page

--- blog/scripts/dokku_static.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-
-// Add .static file to the root to make use of https://github.com/dokku/buildpack-nginx
-hexo.extend.filter.register('after_generate', function () {
-  fs.writeFileSync(path.join(__dirname, '..', 'public', '.static'), '')
-});
