b4c189d663bddad79b9e859465be51f740a41e40
Mateusz Derks <mateusz.derks@toptal.com>
2017-12-05 18:27:55 +0100

    Clean up spaces in tommy_example tag

--- blog/scripts/tommy.js
+++ blog/scripts/tommy.js
@@ -1,9 +1,9 @@
 const BASE_URL = process.env.BASE_TOMMY_URL || 'https://tommy-web.ertrzyiks.me/'
 
 hexo.extend.tag.register('tommy_example', function(args){
-    const slug = args[0]
+  const slug = args[0]
 
-    return `
+  return `
 <div class="post-iframe_placeholder--tommy">
   <div class="post-iframe_wrapper--tommy">
     <button class="post-iframe_button--tommy is-hidden" style="display: none;" data-role="load-exercise">Load exercise</button>
@@ -14,12 +14,12 @@ hexo.extend.tag.register('tommy_example', function(args){
 });
 
 hexo.extend.filter.register('after_render:html', function(str){
-    const hasTommyExample = str.indexOf('post-iframe_placeholder--tommy') >= 0
+  const hasTommyExample = str.indexOf('post-iframe_placeholder--tommy') >= 0
 
-    if (hasTommyExample) {
-        str = str.replace('</head>', '<link rel="stylesheet" type="text/css" href="/content/assets/css/tommy.css"></head>')
-        str = str.replace('</body>', '<script src="/content/assets/js/tommy.js"></script>')
-    }
+  if (hasTommyExample) {
+    str = str.replace('</head>', '<link rel="stylesheet" type="text/css" href="/content/assets/css/tommy.css"></head>')
+    str = str.replace('</body>', '<script src="/content/assets/js/tommy.js"></script>')
+  }
 
-    return str
+  return str
 });
