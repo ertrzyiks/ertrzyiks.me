2c41b229ae6c399ee08d286571fddb7dab60f3f2
ertrzyiks <mateusz.derks@gmail.com>
2021-12-31 14:27:17 +0100

    Update deployment

--- home/scripts/deploy.sh
+++ home/scripts/deploy.sh
@@ -6,7 +6,7 @@ touch ./.git-deploy/.static
 cd .git-deploy
 git init
 pwd
-git remote add dokku dokku@ertrzyiks.me:home
+git remote add dokku dokku@167.71.190.115:home
 git add .
 git commit -m 'Update'
 git push dokku master --force
