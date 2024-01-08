83f8009b4aa7e845b9bcab56424e9b7c9e2f4853
ertrzyiks <mateusz.derks@gmail.com>
2024-01-08 21:29:57 +0100

    Update deps

--- blog/scripts/webpack-dev-server.js
+++ blog/scripts/webpack-dev-server.js
@@ -1,24 +1,27 @@
 (function (startWebpackDevServer) {
   if (hexo.env.args.watch) {
-    startWebpackDevServer()
+    startWebpackDevServer();
   }
 })(function () {
-  const webpack = require('webpack')
-  const WebpackDevServer = require('webpack-dev-server')
-  const config = require('../webpack.config');
-  console.log('Starting the dev web server...');
+  const webpack = require("webpack");
+  const WebpackDevServer = require("webpack-dev-server");
+  const config = require("../webpack.config");
+  console.log("Starting the dev web server...");
   const port = 8080;
 
   const options = {
-    stats: { colors: true }
+    // stats: { colors: true }
   };
 
-  const server = new WebpackDevServer(webpack(config), options);
+  const server = new WebpackDevServer(
+    webpack({ ...config, mode: "development" }),
+    options
+  );
 
-  server.listen(port, 'localhost', function (err) {
+  server.listen(port, "localhost", function (err) {
     if (err) {
       console.log(err);
     }
-    console.log('WebpackDevServer listening at localhost:', port);
+    console.log("WebpackDevServer listening at localhost:", port);
   });
-})
+});
--- blog/webpack.config.js
+++ blog/webpack.config.js
@@ -1,23 +1,24 @@
-require('dotenv').load()
+require("dotenv").load();
 
-const webpack = require('webpack')
-const path = require('path')
-const ExtractCssBlockPlugin = require('extract-css-block-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const { WebpackManifestPlugin: ManifestPlugin } = require('webpack-manifest-plugin')
+const webpack = require("webpack");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const {
+  WebpackManifestPlugin: ManifestPlugin,
+} = require("webpack-manifest-plugin");
 
-const publicAssetsPath = process.env.PUBLIC_ASSETS_PATH || '/'
+const publicAssetsPath = process.env.PUBLIC_ASSETS_PATH || "/";
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: {
-    post: path.resolve(__dirname, './themes/nono/assets/post.js'),
-    output: path.resolve(__dirname, './themes/nono/assets/index.js'),
-    raven: path.resolve(__dirname, './themes/nono/assets/raven.js')
+    post: path.resolve(__dirname, "./themes/nono/assets/post.js"),
+    output: path.resolve(__dirname, "./themes/nono/assets/index.js"),
+    raven: path.resolve(__dirname, "./themes/nono/assets/raven.js"),
   },
   output: {
-    path: path.resolve(__dirname, './themes/nono/source/'),
-    filename: 'js/[name]-[hash].js'
+    path: path.resolve(__dirname, "./themes/nono/source/"),
+    filename: "js/[name]-[hash].js",
   },
   module: {
     rules: [
@@ -25,51 +26,50 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['env']
-          }
-        }
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
       {
         test: /\.sass$/,
         use: [
           MiniCssExtractPlugin.loader,
-          'css-loader?minimize=true',
-          'postcss-loader',
-          'sass-loader'
-        ]
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2)$/,
         use: {
-          loader: 'file-loader',
+          loader: "file-loader",
           options: {
-            outputPath: 'css/fonts/',
-            publicPath: process.env.NODE_ENV == 'production' ? '/' : ''
-          }
-        }
-      }
-    ]
+            outputPath: "css/fonts/",
+            publicPath: process.env.NODE_ENV == "production" ? "/" : "",
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'css/compiled-[hash].css'
+      filename: "css/compiled-[hash].css",
     }),
-    new ExtractCssBlockPlugin(),
     new webpack.DefinePlugin({
       "process.env": {
         SENTRY_PUBLIC_DSN: JSON.stringify(process.env.SENTRY_PUBLIC_DSN),
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
-      }
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || "development"),
+      },
     }),
     new ManifestPlugin({
-      fileName: '../assets.json',
+      fileName: "../assets.json",
       writeToFileEmit: true,
       publicPath: publicAssetsPath,
       seed: {
-        'fallback.css': publicAssetsPath + 'css/fallback.css'
-      }
-    })
-  ]
-}
+        "fallback.css": publicAssetsPath + "css/fallback.css",
+      },
+    }),
+  ],
+};
