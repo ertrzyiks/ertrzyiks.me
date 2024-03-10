00ff60371cf95038afebf478df0680f195e4eb93
ertrzyiks <mateusz.derks@gmail.com>
2024-03-10 20:50:17 +0100

    Update sprites

--- home/scripts/generate_sprites.sh
+++ home/scripts/generate_sprites.sh
@@ -2,23 +2,25 @@
 
 function build {
   sprite_name="$1"
-  file_list_txt=./src/assets/${1}/files.txt
-    echo "> Build ${1}"
+  file_list_txt=./assets/${1}/files.txt
+  echo "> Build ${1}"
 
-    if [[ ! -f "$file_list_txt" ]]; then
-      echo "${file_list_txt} is missing, skipping ${1}"
-      return
-    fi
+  if [[ ! -f "$file_list_txt" ]]; then
+    echo "${file_list_txt} is missing, skipping ${1}"
+    return
+  fi
 
   ALL_PATHS=""
   while read p; do
-    ALL_PATHS="${ALL_PATHS} ./src/assets/all/$p"
-  done <./src/assets/${1}/files.txt
+    ALL_PATHS="${ALL_PATHS} ./assets/all/$p"
+  done < ./assets/${1}/files.txt
 
-  TexturePacker --format json --multipack --trim-sprite-names --sheet "./src/assets/sprites/${sprite_name}-{n}.png" --data "./src/assets/sprites/${sprite_name}-{n}.json" ${ALL_PATHS}
+  echo $ALL_PATHS
+
+  TexturePacker --format json --multipack --trim-sprite-names --sheet "./assets/sprites/${sprite_name}-{n}.png" --data "./assets/sprites/${sprite_name}-{n}.json" ${ALL_PATHS}
 }
 
-for dir in ./src/assets/*
+for dir in ./assets/*
 do
   test -d "$dir" || continue
   dir_name="${dir##*/}"
